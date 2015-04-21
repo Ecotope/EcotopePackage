b3de259cddf7d33a29ba925e4b8f742bccb0d11a
JeffreyUslan <jeffreyu@ecotope.com>
2015-04-21 10:32:41 -0700

    4/21

---
+++ package_making_scratch.R
@@ -0,0 +1,30 @@
+# install.packages("devtools")
+# devtools::install_github("klutometis/roxygen")
+library("devtools")
+library(roxygen2)
+
+
+setwd("E:/R/")
+# create("EcotopePackage")
+# setwd("./EcotopePackage")
+# document()
+# install("EcotopePackage")
+
+install_github('Ecotope/EcotopePackage')
+library(EcotopePackage)
+
+ devtools::document()
+
+
+
+? cdx
+? name_fix
+? var_range_fix
+
+? continuous_uniform_time
+? omit_NA_cols
+? omit_zero_cols
+? omit_NA_or_zero_cols
+? rbind.all.columns
+? single_pt_avg_impute
+? stata_time_to_R_time
